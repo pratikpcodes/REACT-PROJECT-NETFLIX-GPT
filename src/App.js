44e052fca44cc1bc91424fb5a36c3230612126f1
pratikpcodes <pratikkpcodes@gmail.com>
2023-11-09 14:29:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import './index.css';
+import Body from './components/Body';
+import Header from './components/Header';
+function App() {
+  return (
+  <>
+   <Body/>
+   <Header/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/Body.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div className='underline text-blue-400 '>Body</div>
+  )
+}
+
+export default Body
